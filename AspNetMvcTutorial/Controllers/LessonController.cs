3838cb78fd97f48be22f6033c01e2a4ff973260b
m312u <m312u@outlook.jp>
2025-08-11 00:19:28 +0900

    解説を追加

---
+++ AspNetMvcTutorial/Controllers/LessonController.cs
@@ -0,0 +1,52 @@
+﻿// Controllers/LessonController.cs
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMvcTutorial.Controllers
+{
+    public class LessonController : Controller
+    {
+        // 1. Introduction
+        public IActionResult Introduction()
+        {
+            return View();
+        }
+
+        // 2. MVCの基本構造と流れ
+        public IActionResult Routing()
+        {
+            return View();
+        }
+
+        // 3. Controllerの役割とHTTP属性の説明
+        public IActionResult Controller()
+        {
+            return View();
+        }
+
+        // 4. モデルとバリデーション
+        public IActionResult Validation()
+        {
+            return View();
+        }
+
+        // 5. ビューの役割とRazor構文
+        public IActionResult ViewLesson()
+        {
+            return View();
+        }
+
+        // 6. Entity Framework（マイグレーション・最適化）
+        public IActionResult EntityFramework()
+        {
+            return View();
+
+        }
+
+        // 7. 最後に
+        public IActionResult Last()
+        {
+            return View();
+        }
+
+    }
+}
